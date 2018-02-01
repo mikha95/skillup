b8654ae91357e7ea0aee7bfe8836cff344345cbe
SkillUP Student <student@skillup.spb.ru>
2018-02-01 21:55:25 +0300

    add php elements

---
+++ include/head.php
@@ -0,0 +1,6 @@
+<head>
+<title><?= PROJECT_NAME; ?></title>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="image/favicon.ico">
+    <link rel="stylesheet" href="css/style.css">
+</head>
